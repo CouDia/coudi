f8221f22ea130bf3c3e8cf0f516853771a3fce04
CouDia <61709774+CouDia@users.noreply.github.com>
2020-08-08 12:07:40 +0000

    Add files via upload

---
+++ jeu.js
@@ -0,0 +1,48 @@
+var montant = 100;
+var perte =0;
+function jouer() {
+    var number = parseInt( document.getElementById("number").value);
+    var mise = parseInt( document.getElementById("mise").value); 
+    var gagnant = Math.round(Math.random() * 36);
+   
+
+    if (mise > 100) {
+        alert("solde insufisant");
+    } 
+    else if (mise<=0) {
+        alert("mise invalide");
+    }
+    else {
+        montant -= mise;
+
+        if (number == gagnant) {
+            alert("nombre gagnant  = " + gagnant + "     Vous avez gagné " + (mise + mise * 35) + "000 francs");
+            montant += (mise + mise * 35)
+        }
+        else if (gagnant == 0) {
+            alert("nombre gagnant  = " + gagnant + "      Vous avez perdu "+ mise + "000 francs");
+           
+            perte += mise;
+        }
+        else if (number == "pair" && gagnant % 2 == 0) {
+            alert("nombre gagnant  = " + gagnant + "       Vous avez gagné " + mise * 2 + " 000 francs");
+            montant += (mise * 2);
+
+        }
+        else if (number == "impair" && gagnant % 2 ==1) {
+            alert("nombre gagnant  = " + gagnant + "       Vous avez gagné " + mise * 2 + "000 francs");
+            montant += (mise * 2);
+        }
+        else {
+            alert("nombre gagnant  = " + gagnant + "       Vous avez perdu "+ mise + "000 francs");
+            
+            perte += mise;
+        }
+    }
+    if (solde == 0) {
+        alert("VOUS AVEZ ECHOUE !!!")
+    }
+
+    document.getElementById("solde").value = montant;
+    document.getElementById("perte").value = perte;
+}
